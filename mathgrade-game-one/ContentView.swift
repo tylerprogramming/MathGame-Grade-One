8564dd69d84b79958a41790a3c1a278fa51908ce
Tyler Reed <tylerreedytlearning@gmail.com>
2022-05-05 19:18:51 -0400

    Initial Commit

---
+++ mathgrade-game-one/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  mathgrade-game-one
+//
+//  Created by Tyler Reed on 5/5/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ mathgrade-game-one/mathgrade_game_oneApp.swift
@@ -0,0 +1,17 @@
+//
+//  mathgrade_game_oneApp.swift
+//  mathgrade-game-one
+//
+//  Created by Tyler Reed on 5/5/22.
+//
+
+import SwiftUI
+
+@main
+struct mathgrade_game_oneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
